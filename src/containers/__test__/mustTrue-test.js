5e886a4681d141b9a49eee64fbfe64413f6d3398
Hanwen Cheng <heawen.cheng@gmail.com>
2016-01-05 23:49:28 +0100

    add a small test file

---
+++ src/containers/__test__/mustTrue-test.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+
+describe('MustTrue', function() {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+});
