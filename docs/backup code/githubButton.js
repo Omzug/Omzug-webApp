5d5e3cfad942af6eee2315d053e6af5905a99ef8
Hanwen Cheng <heawen.cheng@gmail.com>
2016-01-21 21:52:44 +0100

    add backup folder

---
+++ docs/backup code/githubButton.js
@@ -0,0 +1,16 @@
+/**
+ * Created by hanwencheng on 1/21/16.
+ */
+
+<GithubButton user="hanwencheng"
+              repo="SeekDev"
+              type="star"
+              width={80}
+              height={20}
+              count large/>
+<GithubButton user="hanwencheng"
+repo="SeekDev"
+type="fork"
+width={80}
+height={20}
+count large/>
