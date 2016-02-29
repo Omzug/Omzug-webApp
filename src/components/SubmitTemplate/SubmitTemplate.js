6930edb48e3e36899249f5062ed6793fe355d627
softlipaschara <yangxinyue@yangxinyues-mbp.speedport_w_724v_typ_a_05011603_00_007>
2016-02-29 18:00:57 +0100

    fix image size

--- src/components/SubmitTemplate/SubmitTemplate.js
+++ src/components/SubmitTemplate/SubmitTemplate.js
@@ -88,7 +88,7 @@ export default class SubmitTemplate extends Component {
             */}
             <CardMedia style={uiStyles.CardMedia}>
               <div>
-                <Carousel className={styles.slider} decorators={Decorators} framePadding="50px" width="100%" slidesToShow={1}>
+                <Carousel className={styles.carousel} decorators={Decorators} framePadding="32px" width="100%" slidesToShow={1}>
                   {entity.images.length >= 1 && entity.images.map(address => (<div className={styles.imageContainer}><img src={address}/></div>))}
                   {cachedImages.length >= 1 && cachedImages.map(file => <div className={styles.imageContainer}><img src={window.URL.createObjectURL(file)}/></div>)}
                   {entity.images.length == 0 && cachedImages.length == 0 &&  <div className={styles.imageContainer}><img src={config.noImagePath}/></div>}
