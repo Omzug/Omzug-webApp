b52dad2aade9a99691f0d8178ff1420cf8ec6efd
softlipaschara <yangxinyue@yangxinyues-mbp.speedport_w_724v_typ_a_05011603_00_007>
2016-03-01 00:12:44 +0100

    submitTemplate redesigned responsive too

--- src/components/SubmitTemplate/SubmitTemplate.js
+++ src/components/SubmitTemplate/SubmitTemplate.js
@@ -78,15 +78,8 @@ export default class SubmitTemplate extends Component {
     return (
       <div className={styles.container}>
         <div className={styles.card}>
-          <Card style={uiStyles.card}>
-            {/*
-            <CardHeader
-              title="房子的标题"
-              subtitle="房子的副标题"
-              avatar="http://lorempixel.com/100/100/nature/"
-            />
-            */}
-            <CardMedia style={uiStyles.CardMedia}>
+
+            <div className={styles.cardMedia}>
               <div>
                 <Carousel className={styles.carousel} decorators={Decorators} framePadding="32px" width="100%" slidesToShow={1}>
                   {entity.images.length >= 1 && entity.images.map(address => (<div className={styles.imageContainer}><img src={address}/></div>))}
@@ -94,46 +87,49 @@ export default class SubmitTemplate extends Component {
                   {entity.images.length == 0 && cachedImages.length == 0 &&  <div className={styles.imageContainer}><img src={config.noImagePath}/></div>}
                 </Carousel>
               </div>
-            </CardMedia>
+
+
             <div className={styles.cardTitle}>
-              <div><CardTitle style={uiStyles.CardTitle} title={entity.title} subtitle={"by " + entity.username} /></div>
+              <div className={styles.cardTitleTitle}>{entity.title}</div>
+              <div className={styles.cardTitleUsername}>by {entity.username}</div>
             </div>
             <div className={styles.cardText}>
-              <div><CardText style={uiStyles.CardText}>
-                {entity.description}
-              </CardText></div>
+              {entity.description}
             </div>
 
-            <CardActions>
-              <FlatButton style={uiStyles.actionButton} onClick={this.props.onContactOpen}><span className="fa fa-envelope"/> 联系房主</FlatButton>
-              {/*<FlatButton style={uiStyles.actionButton}
-                          className={styles.button} onClick={this.props.onContactOpen}><span className="fa fa-share"/> 分享</FlatButton>*/}
 
-            <div className={styles.dialog}>
-              <Dialog
-                actions={
-                <div>
-                  <FlatButton onClick={this.props.onContactClose} className={styles.hvrBuzzOut}>
-                    <span className="fa fa-child"/>
-                    <span>  </span>OK
-                  </FlatButton>
-                </div>
-                }
+            <div className={styles.cardActions}>
+              <div className={styles.contactHost}>
+                <FlatButton style={uiStyles.actionButton} onClick={this.props.onContactOpen}><span className="fa fa-envelope"/> 联系房主</FlatButton>
+              </div>
 
-                modal={false}
-                open={contactOpen}
-                onRequestClose={this.props.onContactClose}
-              >
-                <div className={styles.contactInfo}>
-                  <div className={styles.infoTitle}> {entity.username}的联系方式:</div>
-                  <div className={styles.infoListMail}> <i className="fa fa-envelope-o" />  邮箱: &nbsp; {entity.email} </div>
-                  <div className={styles.infoListPhone}> <i className="fa fa-phone" />  手机: &nbsp; {entity.phone} </div>
-                </div>
-              </Dialog>
+              <div className={styles.dialog}>
+                <Dialog
+                  actions={
+                  <div>
+                    <FlatButton onClick={this.props.onContactClose} className={styles.hvrBuzzOut}>
+                      <span className="fa fa-child"/>
+                      <span>  </span>OK
+                    </FlatButton>
+                  </div>
+                  }
+
+                  modal={false}
+                  open={contactOpen}
+                  onRequestClose={this.props.onContactClose}
+                >
+                  <div className={styles.contactInfo}>
+                    <div className={styles.infoTitle}> {entity.username}的联系方式:</div>
+                    <div className={styles.infoListMail}> <i className="fa fa-envelope-o" />  邮箱: &nbsp; {entity.email} </div>
+                    <div className={styles.infoListPhone}> <i className="fa fa-phone" />  手机: &nbsp; {entity.phone} </div>
+                  </div>
+                </Dialog>
+              </div>
+
             </div>
+          </div>
 
-            </CardActions>
-          </Card>
+
           { entity.lat && entity.lng &&
             <div className={styles.map}>
               <Map geometry={[entity.lat, entity.lng]}/>
@@ -156,39 +152,9 @@ export default class SubmitTemplate extends Component {
           </div>
 
 
+
         </div>
 
-        {/*<List className={styles.list}>
-          <ListItem key={1} className="hint--top" data-hint="城市" primaryText={entity.city} leftIcon={<FontIcon className="fa fa-map-marker" />} />
-          <ListItem key={2} className="hint--top" data-hint="地址" primaryText={entity.location} leftIcon={<FontIcon className="fa fa-map" />} />
-          <ListItem key={3} className="hint--top" data-hint="房间数" primaryText={entity.roomNumber} leftIcon={<FontIcon className="fa fa-codepen" />} />
-          <ListItem key={4} className="hint--top" data-hint="面积" primaryText={entity.size}  leftIcon={<FontIcon className="fa fa-th" />}/>
-          <ListItem key={5} className="hint--top" data-hint="租金" primaryText={entity.price}  leftIcon={<FontIcon className="fa fa-euro" />}/>
-          <ListItem key={6} className="hint--top" data-hint="押金" primaryText={entity.caution}  leftIcon={<FontIcon className="fa fa-money" />}/>
-          <ListItem key={7} className="hint--top" data-hint="最多人数" primaryText={entity.maximumPerson}  leftIcon={<FontIcon className="fa fa-child" />}/>
-          <ListItem key={8} className="hint--top" data-hint="类型" primaryText={entity.type? "整套出租" : "单间"}  leftIcon={<FontIcon className="fa fa-home" />}/>
-          <ListItem key={9} leftIcon={<FontIcon className="fa fa-calendar" />} children={
-          <div>
-            <p className="hint--top" data-hint="开始日期">{formatDate(entity.startDate)}</p>
-            <br/>
-            <p className="hint--top" data-hint="结束日期">{entity.endDate? formatDate(entity.endDate): "无限制"}</p>
-          </div>
-          }>
-          </ListItem>
-
-
-
-          {entity.note &&
-            <ListItem key={11} className={styles.note} zDepth={2} className="hint--top" data-hint="备注">
-              <p className={styles.note}>{entity.note}</p>
-            </ListItem>
-          }
-
-          {userId && userId == entity.owner &&
-          <RaisedButton key={12} className={styles.editButton} onClick={this.props.onStartEdit}><span
-            className="fa fa-pencil"/> 编辑</RaisedButton>
-          }
-        </List>*/}
       </div>
     );
   }
