14c6ccbc62244801fbd42032a3e085b1b6df039d
Hanwen Cheng <heawen.cheng@gmail.com>
2016-03-01 21:29:09 +0100

    update sitemap

--- api/lib/sitemap.js
+++ api/lib/sitemap.js
@@ -8,10 +8,10 @@ var sitemap = sm.createSitemap ({
   hostname: 'http://omzug.com',
   cacheTime: 600000,        // 600 sec - cache purge period
   urls: [
-    { url: '/主页/',  changefreq: 'daily', priority: 0.7 },
-    { url: '/登录/',  changefreq: 'monthly',  priority: 0.1 },
-    { url: '/注册/', changefreq: 'monthly', priority: 0.1 },    // changefreq: 'weekly',  priority: 0.5
-    { url: '/关于我们/', changefreq: 'monthly', priority: 0.1 }, // img: "http://urlTest.com"
+    { url: '/',  changefreq: 'daily', priority: 0.7 },
+    { url: '/register',  changefreq: 'monthly',  priority: 0.1 },
+    { url: '/login', changefreq: 'monthly', priority: 0.1 },    // changefreq: 'weekly',  priority: 0.5
+    { url: '/about', changefreq: 'monthly', priority: 0.1 }, // img: "http://urlTest.com"
   ]
 });
 
