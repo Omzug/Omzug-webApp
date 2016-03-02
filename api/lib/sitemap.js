abb2f4119bf34c2ac48be4c89eeb2893f0041009
Hanwen Cheng <heawen.cheng@gmail.com>
2016-03-02 22:24:33 +0100

    change sitemap

--- api/lib/sitemap.js
+++ api/lib/sitemap.js
@@ -8,7 +8,8 @@ var sitemap = sm.createSitemap ({
   hostname: 'http://omzug.com',
   cacheTime: 600000,        // 600 sec - cache purge period
   urls: [
-    { url: '/',  changefreq: 'daily', priority: 0.7 },
+    { url: '/',  changefreq: 'daily', priority: 0.4 },
+    { url: '/main', changefreq : 'daily', priority : 0.3},
     { url: '/register',  changefreq: 'monthly',  priority: 0.1 },
     { url: '/login', changefreq: 'monthly', priority: 0.1 },    // changefreq: 'weekly',  priority: 0.5
     { url: '/about', changefreq: 'monthly', priority: 0.1 }, // img: "http://urlTest.com"
