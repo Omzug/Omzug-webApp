4d12df99eee4e751ff938c04b13d2d2608eb3a47
softlipaschara <yangxinyue@yangxinyues-mbp.speedport_w_724v_typ_a_05011603_00_007>
2016-03-03 23:42:18 +0100

    发布房屋 title往上提了一点
    最多上传三张图片变红

--- src/components/SubmitForm/SubmitForm.js
+++ src/components/SubmitForm/SubmitForm.js
@@ -151,7 +151,7 @@ export default class SubmitForm extends Component {
         }
       }
       if(anyError.length){
-        return this.props.onLogError("未填写" + anyError.join(", "));
+        return this.props.onLogError("未填写"  + anyError.join(", "));
       }
       handleSubmit(data)
     }
@@ -178,7 +178,7 @@ export default class SubmitForm extends Component {
                 <div className={styles.imageContainer}>
                   <DropZone onDrop={this.onDrop}>
                     <div className={styles.inner}>
-                      <div className={styles.innerText}>请点击选择图片或者将图片拖动到框中,最多上传三张图片</div>
+                      <div className={styles.innerText}>请点击选择图片或将图片拖动到框中,<font color="#FF6F6F">最多上传<b><font>3</font></b>张图片</font></div>
                       <div className={styles.innerFont}>
                         <span className="fa fa-plus-circle fa-5x"/>
                       </div>
@@ -188,10 +188,12 @@ export default class SubmitForm extends Component {
               }
             </Carousel>
           </CardMedia>
-          <CardTitle>
-              {/* directly display the require error here since it hard to find */}
-            <TextField key={201} hintText="标题" floatingLabelText="标题" errorText={title.touched && title.error ? title.error : null} {...title}/>
-          </CardTitle>
+          <div className={styles.cardTitle}>
+            <CardTitle>
+                {/* directly display the require error here since it hard to find */}
+              <TextField key={201} hintText="标题" floatingLabelText="标题" errorText={title.touched && title.error ? title.error : null} {...title}/>
+            </CardTitle>
+          </div>
           <CardText style={uiStyles.cardText}>
               <textarea key={202} className={"form-control " + styles.textArea} rows="8" placeholder="填写一些具体介绍吧" {...description}/>
           </CardText>
