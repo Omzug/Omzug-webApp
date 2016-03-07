eafaf3ea24bdf9958e005a0bc66a219580a2c2e1
softlipaschara <yangxinyue@computer.local>
2016-03-07 23:58:55 +0100

    featureImage size much smaller
    smaller than 700k now

--- src/constant/imageAddress.js
+++ src/constant/imageAddress.js
@@ -7,7 +7,8 @@ const addresses = {
   loginImage: "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/loginImage-omzug.jpg",
   registerImage : "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/registerImage-omzug.jpg",
   //featureImage :"https://s3.eu-central-1.amazonaws.com/omzug.com/gif/featureImage-omzug1500.png",
-  featureImage:"https://40.media.tumblr.com/064e5c00948df4933282d65c01fd3a0d/tumblr_o3ow8mj70r1vn5ujpo1_1280.png",
+  //featureImage:"https://40.media.tumblr.com/064e5c00948df4933282d65c01fd3a0d/tumblr_o3ow8mj70r1vn5ujpo1_1280.png",
+  featureImage:"https://41.media.tumblr.com/8ec96e540a32e7f5e47ec212de211571/tumblr_o3oybnlWdg1vn5ujpo1_1280.png",
   technologyImage : "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/technologyImage-omzug.png",
   bulbImage:"https://41.media.tumblr.com/91005ecc75a3ddc9541c68688b5f806c/tumblr_o3owzwfJZX1vn5ujpo1_75sq.png",
 }
