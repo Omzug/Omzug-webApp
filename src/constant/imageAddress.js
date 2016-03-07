ca9007c85a845beb88a8e93059a3ed18ba3dde82
softlipaschara <yangxinyue@computer.local>
2016-03-07 23:50:35 +0100

    home new illustration added
    a computer sketch and a bulb sketch

--- src/constant/imageAddress.js
+++ src/constant/imageAddress.js
@@ -6,8 +6,10 @@ const addresses = {
   mainImage: "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/mainImage-omzug.gif",
   loginImage: "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/loginImage-omzug.jpg",
   registerImage : "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/registerImage-omzug.jpg",
-  featureImage :"https://s3.eu-central-1.amazonaws.com/omzug.com/gif/featureImage-omzug1500.png",
+  //featureImage :"https://s3.eu-central-1.amazonaws.com/omzug.com/gif/featureImage-omzug1500.png",
+  featureImage:"https://40.media.tumblr.com/064e5c00948df4933282d65c01fd3a0d/tumblr_o3ow8mj70r1vn5ujpo1_1280.png",
   technologyImage : "https://s3.eu-central-1.amazonaws.com/omzug.com/gif/technologyImage-omzug.png",
+  bulbImage:"https://41.media.tumblr.com/91005ecc75a3ddc9541c68688b5f806c/tumblr_o3owzwfJZX1vn5ujpo1_75sq.png",
 }
 
 export default addresses;
