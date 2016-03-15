8c62cf65ee9a605d1069c8e4f900c372f9dada51
Hanwen Cheng <heawen.cheng@gmail.com>
2016-03-15 18:33:35 +0100

    complete star function, but every time it changes, the session should also change, or other method?

--- api/actions/index.js
+++ api/actions/index.js
@@ -5,6 +5,7 @@ export loadAuth from './auth/loadAuth';
 export login from './auth/login';
 export logout from './auth/logout';
 export register from './auth/register';
+export toggleStar from './auth/toggleStar';
 
 export list from './house/list';
 export house from './house/house';
