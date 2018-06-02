74cb3bfa6cc62aec59d0ffadc4b1e59f791f33cd
Hanwen Cheng <heawen.cheng@gmail.com>
2018-06-02 23:19:51 +0200

    remove uglify

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -77,13 +77,13 @@ module.exports = {
     }),
 
     // optimizations
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
 
     webpackIsomorphicToolsPlugin
   ]
